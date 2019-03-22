2dce7644eba290d8c5b1174cb2d5ed2af1d60e37
hsvgbkhgbv <wangjianhong1993@gmail.com>
2019-03-22 12:35:15 +0000

    jianhong

--- run.py
+++ run.py
@@ -44,30 +44,32 @@ Args = namedtuple('Args', ['agent_num',
                  )
 
 args = Args(agent_num=env.get_num_of_agents(),
-            hid_size=64,
+            hid_size=128,
             obs_size=np.max(env.get_shape_of_obs()),
             continuous=False,
             action_dim=np.max(env.get_output_shape_of_act()),
-            comm_iters=10,
+            comm_iters=1,
             init_std=0.01,
             lrate=1e-5,
-            batch_size=128,
-            max_steps=1000,
+            batch_size=1024,
+            max_steps=2000,
             gamma=0.99,
             mean_ratio=0.0,
             normalize_rewards=False,
             advantages_per_action=False,
             value_coeff=0.0,
-            entr=0.0,
+            entr=1e-5,
             action_num=np.max(env.get_input_shape_of_act())
            )
 
 policy_net = CommNet(args)
-num_epoch = 10000
+num_epoch = 1000
 epoch = 0
 for i in range(num_epoch):
     train = Trainer(args, policy_net, env())
     train.train_batch()
     print ('This is the epoch: {}, the time step is {} and the current advantage is: {}'.format(epoch, train.stats['num_steps'], train.stats['action_loss']))
     epoch += 1
-torch.save(policy_net, './exp1/coop.pt')
+    if i%10 == 0:
+        print ('The model is saved!')
+        torch.save(policy_net, './exp1/coop.pt')
