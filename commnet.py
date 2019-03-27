c22206618a6f8555e5eb12f1f2103486e372dd80
Jianhong Wang <jw11815@ee-jw11815.ee.ic.ac.uk>
2019-03-27 14:22:41 +0000

    jianhong

--- commnet.py
+++ commnet.py
@@ -54,6 +54,7 @@ class CommNet(nn.Module):
         self.encoder = nn.Linear(self.args.obs_size, self.args.hid_size)
         # communication mask where the diagnal should be 0
         self.comm_mask = torch.ones(self.args.agent_num, self.args.agent_num) - torch.eye(self.args.agent_num, self.args.agent_num)
+        self.comm_mask.cuda() if torch.cuda.is_available()
         # decoder transforms hidden states to action vector
         if self.args.continuous:
             self.action_mean = nn.Linear(self.args.hid_size, self.args.action_dim)
