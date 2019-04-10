238a04d3f7fadcc1a4bc2e5c73ae6d1e81549d98
Jianhong Wang <jw11815@ee-jw11815.ee.ic.ac.uk>
2019-04-10 13:34:44 +0100

    update

--- train.py
+++ train.py
@@ -16,21 +16,15 @@ model = model_map[model_name]
 
 train = Trainer(args, model, env())
 
-epoch = 0
-
 for i in range(args.train_epoch_num):
     train.train_batch(i)
-    print ('This is the epoch: {}, the mean reward is {:2.4f} and the current action loss to be minimized is: {:2.4f}\n'.format(epoch, train.stats['mean_reward'], train.stats['action_loss']))
-    epoch += 1
-
-    ### Tensorboard Logging ###
+    print ('This is the epoch: {}, the mean reward is {:2.4f} and the current action loss to be minimized is: {:2.4f}\n'.format(i, train.stats['mean_reward'], train.stats['action_loss']))
     info = {'mean_reward': train.stats['mean_reward'],'action_loss': train.stats['action_loss'] }
     for tag, value in info.items():
-        logger.scalar_summary(tag, value, epoch)
-
+        logger.scalar_summary(tag, value, i)
     if i%args.target_update_freq == args.target_update_freq-1:
         torch.save({'model_state_dict': train.behaviour_net.state_dict()}, './exp1/' + scenario_name + '_' + args.training_strategy + '_' + model_name + '.pt')
         print ('The model is saved!\n')
         with open('./exp1/' + scenario_name + '_' + args.training_strategy + '_' + model_name + '.log', 'w+') as file:
             file.write(str(args)+'\n')
-            file.write(str(epoch))
+            file.write(str(i))
