6c13a39b5ec2fbb9a46ac7bd36e2ce4174e41093
yuanzhang <mike.zy7@gmail.com>
2019-04-10 05:25:49 +0100

    add tensorboard

--- train.py
+++ train.py
@@ -7,6 +7,9 @@ import os
 from ic3net import *
 from util import *
 
+# tensorboard file
+from logger import Logger
+logger = Logger('./logs')
 
 model = model_map[model_name]
 
@@ -18,6 +21,12 @@ for i in range(args.train_epoch_num):
     train.train_batch(i)
     print ('This is the epoch: {}, the mean reward is {:2.4f} and the current action loss to be minimized is: {:2.4f}\n'.format(epoch, train.stats['mean_reward'], train.stats['action_loss']))
     epoch += 1
+
+    ### Tensorboard Logging ###
+    info = {'mean_reward': train.stats['mean_reward'],'action_loss': train.stats['action_loss'] }
+    for tag, value in info.items():
+        logger.scalar_summary(tag, value, epoch)
+
     if i%10 == 9:
         torch.save({'model_state_dict': train.behaviour_net.state_dict()}, './exp1/' + scenario_name + '_' + args.training_strategy + '_' + model_name + '.pt')
         print ('The model is saved!\n')
