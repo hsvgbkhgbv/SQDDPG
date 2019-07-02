ed36bb4c8eccf5694ba38956cc27b0395d861f65
huwei <huwei@laiye.com>
2019-07-02 16:03:32 +0000

    update pp_env branch

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
 
-EXP_NAME="network_congestion_independent"
+EXP_NAME="traffic_junction_independent"
+ALIAS="_medium"
 
 cp ./args/$EXP_NAME.py arguments.py
-CUDA_VISIBLE_DEVICES=0 python -u train.py > $EXP_NAME.log
+CUDA_VISIBLE_DEVICES=0 python -u train.py > $EXP_NAME$ALIAS.log
 # mv $EXP_NAME.log ./model_save/$EXP_NAME/train.log
